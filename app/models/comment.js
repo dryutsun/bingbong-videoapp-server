0319087e23105b9f4cf995b369d715f0c8017979
Goro <goro.ikeda.iyeki@gmail.com>
2021-12-11 00:29:07 -0500

    style(comment, readme, boilerplate): remove unnessecary comments and test personal branch git push
    Looking to test the git push origin goro-branch.

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,32 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema(
-    {
-        owner: {
-            // to create a reference, the type should be Object
-            type:mongoose.Schema.Types.ObjectId,
-            // ref is also needed, so we can poulate the owner
-            ref: 'User',
-            // Note: Populate means replacing the owner id with the person document...
+  {
+    owner: {
+      // to create a reference, the type should be Object
+      type: mongoose.Schema.Types.ObjectId,
+      // ref is also needed, so we can poulate the owner
+      ref: "User",
+      // Note: Populate means replacing the owner id with the person document...
 
-            required: true,
-        },
-        display_text:{
-            required: true
-        },
-
-        thumbnail: String, // Maybe better to pull it from the owner.
-
-        published_at:{
-            createdAt: true
-        }
-
+      required: true,
     },
-    {
-        timestamps:true
-    }
-    
-)
+    display_text: {
+      required: true,
+    },
 
-const Comments = mongoose.model("Comment", commentSchema)
-module.exports = Comments
+    thumbnail: String, // Maybe better to pull it from the owner.
+
+    published_at: {
+      createdAt: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comments = mongoose.model("Comment", commentSchema);
+module.exports = Comments;
