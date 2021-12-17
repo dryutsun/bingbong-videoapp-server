07bfa8b69318d401cf1a591d57c742093fedc395
Goro <goro.ikeda.iyeki@gmail.com>
2021-12-17 16:35:40 -0500

    remove cors params

--- server.js
+++ server.js
@@ -45,9 +45,7 @@ const app = express()
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
 app.use(
-	cors({
-		origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`,
-	})
+	cors()
 )
 
 // define port for API to run on
--- upstreamtest.md
+++ upstreamtest.md
@@ -1,1 +1,3 @@
 # bingbong-video-client
+
+// 1. Everytime we pull from upstream immediately push to heroku 
